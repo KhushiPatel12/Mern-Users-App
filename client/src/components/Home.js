afc2da789b0b6c11b6c5552d9253fc284324f95a
KhushiPatel12 <92209863+KhushiPatel12@users.noreply.github.com>
2022-01-05 22:42:16 +0530

    Add files via upload

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+  return(
+    <section>
+        <div className='container-fuild'>
+            <h1>Welcome to Home Page</h1>
+        </div>
+    </section>
+  );
+}
+
+export default Home;
